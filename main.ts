84d90458ac736cf6b90e3efb6cb866e405692eed
Manuel Gómez <m.gomez49@uniandes.edu.co>
2023-10-22 18:03:05 -0500

    Taller_2_todo_funcional

--- main.ts
+++ main.ts
@@ -5,19 +5,95 @@ const seriesTbody: HTMLElement = document.getElementById('tabla_series')!; // No
 const totalCreditElm: HTMLElement = document.getElementById("season_average")!;
 
 
+
 totalCreditElm.innerHTML = `${getSeasonAverage(series)}`
 
 renderCoursesInTable(series);
+
+
 function renderCoursesInTable(series: Serie[]): void {
   series.forEach(serie => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${serie.id}</td>
-                           <td>${serie.name}</td>
+                           <td><a href="#" data-name="${serie.name}">${serie.name}</a></td>
                            <td>${serie.channel}</td>
                            <td>${serie.seasons}</td>`;
     seriesTbody.appendChild(trElement);
+
+    // Agregar un eventListener a cada enlace
+    const links = trElement.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', () => myFunction(serie));
+    });
   });
 }
+
+
+let tarjetaActual: HTMLDivElement | null = null;
+
+function myFunction(serie: Serie) {
+  // Cambia el tamaño de la columna
+  const columnaTabla = document.getElementsByClassName("col-12")[0];
+
+  // Verifica si el elemento tiene la clase "col-12" antes de cambiarla
+  if (columnaTabla && columnaTabla.classList.contains("col-12")) {
+    // Cambia el tamaño de la columna solo si tiene la clase "col-12"
+    columnaTabla.className = "col-6";
+  }
+
+  // Si ya existe una tarjeta, actualiza sus datos en lugar de crear una nueva
+  if (tarjetaActual) {
+    // Actualiza los datos de la tarjeta actual con los de la nueva serie
+    const cardTitle = tarjetaActual.querySelector('.card-title');
+    const cardChannel = tarjetaActual.querySelector('.card-channel');
+    const cardSeasons = tarjetaActual.querySelector('.card-seasons');
+    const cardImage = tarjetaActual.querySelector('.card-img-top');
+    const linkButton = tarjetaActual.querySelector('.btn');
+
+    if (cardTitle) {
+      cardTitle.innerHTML = serie.name;
+    }
+    if (cardChannel) {
+      cardChannel.innerHTML = "Descripción: " + serie.description;
+    }
+    if(cardImage){
+      cardImage.setAttribute("src", serie.image);
+      cardImage.setAttribute("alt", serie.name);
+    }
+    if (linkButton) {
+      // Agrega el enlace al botón "Conoce más" y abre en una nueva ventana
+      linkButton.innerHTML = "Conoce más";
+      linkButton.setAttribute("href", serie.link);
+      linkButton.setAttribute("target", "_blank");
+
+      // Agregar el evento click al botón para abrir el enlace
+      linkButton.addEventListener('click', function(e) {
+        e.preventDefault(); // Evita la acción predeterminada
+        window.open(serie.link, '_blank'); // Abre el enlace en una nueva ventana
+      });
+    }
+  } else {
+    // Si no existe una tarjeta, crea una nueva
+    tarjetaActual = document.createElement("div");
+    tarjetaActual.className = "col-md-4";
+    tarjetaActual.innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <img src="${serie.image}" class="card-img-top" alt="${serie.name}">
+        <div class="card-body">
+          <h5 class="card-title">${serie.name}</h5>
+          <p class="card-text card-channel">Descripción: ${serie.description}</p>
+          <a href="${serie.link}" target="_blank" class="btn btn-primary">Conoce más</a>
+        </div>
+      </div>`;
+  
+    // Agrega la tarjeta a la fila
+    document.getElementsByClassName("row")[0].appendChild(tarjetaActual);
+  }
+}
+
+
+
+
 
 function getSeasonAverage(series: Serie[]): number {
   let totalSeasons: number = 0;
@@ -27,4 +103,3 @@ function getSeasonAverage(series: Serie[]): number {
   const promedio: number = totalSeasons / numSeries;
   return promedio;
 }
-
--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,62 @@ renderCoursesInTable(series);
 function renderCoursesInTable(series) {
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td>").concat(serie.name, "</td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                           <td><a href=\"#\" data-name=\"").concat(serie.name, "\">").concat(serie.name, "</a></td>\n                           <td>").concat(serie.channel, "</td>\n                           <td>").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
+        // Agregar un eventListener a cada enlace
+        var links = trElement.querySelectorAll('a');
+        links.forEach(function (link) {
+            link.addEventListener('click', function () { return myFunction(serie); });
+        });
     });
+}
+var tarjetaActual = null;
+function myFunction(serie) {
+    // Cambia el tamaño de la columna
+    var columnaTabla = document.getElementsByClassName("col-12")[0];
+    // Verifica si el elemento tiene la clase "col-12" antes de cambiarla
+    if (columnaTabla && columnaTabla.classList.contains("col-12")) {
+        // Cambia el tamaño de la columna solo si tiene la clase "col-12"
+        columnaTabla.className = "col-6";
+    }
+    // Si ya existe una tarjeta, actualiza sus datos en lugar de crear una nueva
+    if (tarjetaActual) {
+        // Actualiza los datos de la tarjeta actual con los de la nueva serie
+        var cardTitle = tarjetaActual.querySelector('.card-title');
+        var cardChannel = tarjetaActual.querySelector('.card-channel');
+        var cardSeasons = tarjetaActual.querySelector('.card-seasons');
+        var cardImage = tarjetaActual.querySelector('.card-img-top');
+        var linkButton = tarjetaActual.querySelector('.btn');
+        if (cardTitle) {
+            cardTitle.innerHTML = serie.name;
+        }
+        if (cardChannel) {
+            cardChannel.innerHTML = "Descripción: " + serie.description;
+        }
+        if (cardImage) {
+            cardImage.setAttribute("src", serie.image);
+            cardImage.setAttribute("alt", serie.name);
+        }
+        if (linkButton) {
+            // Agrega el enlace al botón "Conoce más" y abre en una nueva ventana
+            linkButton.innerHTML = "Conoce más";
+            linkButton.setAttribute("href", serie.link);
+            linkButton.setAttribute("target", "_blank");
+            // Agregar el evento click al botón para abrir el enlace
+            linkButton.addEventListener('click', function (e) {
+                e.preventDefault(); // Evita la acción predeterminada
+                window.open(serie.link, '_blank'); // Abre el enlace en una nueva ventana
+            });
+        }
+    }
+    else {
+        // Si no existe una tarjeta, crea una nueva
+        tarjetaActual = document.createElement("div");
+        tarjetaActual.className = "col-md-4";
+        tarjetaActual.innerHTML = "\n      <div class=\"card\" style=\"width: 18rem;\">\n        <img src=\"".concat(serie.image, "\" class=\"card-img-top\" alt=\"").concat(serie.name, "\">\n        <div class=\"card-body\">\n          <h5 class=\"card-title\">").concat(serie.name, "</h5>\n          <p class=\"card-text card-channel\">Descripci\u00F3n: ").concat(serie.description, "</p>\n          <a href=\"").concat(serie.link, "\" target=\"_blank\" class=\"btn btn-primary\">Conoce m\u00E1s</a>\n        </div>\n      </div>");
+        // Agrega la tarjeta a la fila
+        document.getElementsByClassName("row")[0].appendChild(tarjetaActual);
+    }
 }
 function getSeasonAverage(series) {
     var totalSeasons = 0;
